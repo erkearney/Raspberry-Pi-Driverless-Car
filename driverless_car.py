81060ee1a79a9c2035d75a86a11f29d32eb2ab2d
eric <ericrkearney@gmail.com>
2018-12-20 17:17:44 -0700

    Cleaned up decorators

--- driverless_car.py
+++ driverless_car.py
@@ -1,8 +1,8 @@
 # Decorators
 from functools import wraps
 def logger(orig_func):
-    """ This decorator will log which arguments orig_function was called with,
-    out to a file called orig_function.log """
+    """ This decorator will log which arguments orig_func was called with out 
+    to a file called orig_func.log """
     import logging
     logging.basicConfig(filename="{}.log".format(orig_func.__name__), 
                                                  level=logging.INFO)
@@ -14,17 +14,17 @@ def logger(orig_func):
 
     return wrapper
 
-def timer(orig_function):
-    """ This decorator will calculate and print how long orig_function took
+def timer(orig_func):
+    """ This decorator will calculate and print how long orig_func took
     to run """
     import time
 
-    @wraps(orig_function)
+    @wraps(orig_func)
     def wrapper(*args, **kwargs):
-        t1 = time.time()
-        result = orig_function(*args, **kwargs)
-        t2 = time.time() - t1
-        print("{} ran in: {} seconds".format(orig_function.__name__, t2))
+        t = time.time()
+        result = orig_func(*args, **kwargs)
+        t = time.time() - t
+        print("{} ran in: {} seconds".format(orig_func.__name__, t))
         return result
 
     return wrapper
